d67efb25f1c5fdcf3e057fa718f08ffa133f3d91
redgriftjosh <redgriftjosh@gmail.com>
2024-06-03 17:50:55 -0400

    Bug Fix - Creating New Scenarios

--- src/routes/new_scenario.py
+++ src/routes/new_scenario.py
@@ -82,7 +82,10 @@ def clean_json(my_json, processed_ids):
         "section_5_1",
         "section_5",
         "trim",
-        "appendix_graph"
+        "appendix_graph",
+        "appendix_page",
+        "page",
+        "sections"
         ]
 
     for key in keys_to_remove:
