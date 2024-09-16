46a9a4366b2b2a26cde8765f72d5a22ce2a7fcec
redgriftjosh <redgriftjosh@gmail.com>
2024-09-16 11:52:42 -0400

    correct command line note to run ngrok

--- src/script.py
+++ src/script.py
@@ -132,6 +132,6 @@ def run_backend():
 
 
 # ONLY FOR TESTING LOCALLY COMMENT OUT WHEN YOU COMMIT
-# ./ngrok http --domain=up-marmot-tops.ngrok-free.app 3000
+# ngrok http --domain=up-marmot-tops.ngrok-free.app 3000
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
